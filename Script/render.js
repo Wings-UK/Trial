6bf784ca2b64b2bf86a94152cd1d2be1532c336e
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-02-23 21:46:36 +0100

    retry retry

--- Script/render.js
+++ Script/render.js
@@ -120,7 +120,7 @@ function renderHomepage() {
         </div>
         `;
 
-        postContainer.innerHTML += postHTML;
+        postContainer.innerHTML = postHTML;
     });
 }
 renderHomepage();
