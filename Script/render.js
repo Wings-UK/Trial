5ce1ea6c6fce5417750131378ee3d340436255d9
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-02-24 11:46:17 +0100

    goback now!!

--- Script/render.js
+++ Script/render.js
@@ -343,17 +343,5 @@ function goBack() {
 }
 
 
-window.addEventListener("load", function () {
-    let currentPage = location.hash.substring(1);
 
-    if (!currentPage) {
-        currentPage = "food";
-    }
-
-    document.querySelectorAll(".page").forEach(page => {
-        page.classList.remove("active");
-    });
-
-    document.getElementById(currentPage).classList.add("active");
-})
 renderHomepage();
