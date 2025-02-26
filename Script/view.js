c863b00cfd4a13edbe3760ca65aadde3bf57900e
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-02-26 16:01:06 +0100

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -551,7 +551,7 @@ function shortenText(text, limit) {
         shortened = shortened.slice(0, lastSpace); // Cut at last whole word
     }
 
-    return shortened + `... learn more`; // Add ellipsis
+    return shortened + `...<br><class="reer">see more</span>`; // Add ellipsis
 }
  
  window.onpopstate = function (event) {
