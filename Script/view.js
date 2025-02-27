44eb75b93ec7201810c579db22c0223f175b8c5a
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-02-27 18:30:27 +0100

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -40,7 +40,7 @@ const posts = [
       id: 1,
       userId: 1,  // Refers to user with id 1 (@reddcinema)
       timestamp: "11 mins ago",
-      video: "pics/mumu.mp4",
+      video: "pics/css.mp4",
       date: "Feb 28, 2025 3:56 PM",
       content: "Right y'all, I’ve been dating a 36 year old for almost 6 months. I turn 20 in 5 days. How do I tell my parents about it? Have I mentioned he lives 5 states away? 💀💀",
     },
