f36f0f37db4dacbec92a94c17f4d0699f1a75eeb
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-02-28 16:06:09 +0100

    Outline

--- Script/view.js
+++ Script/view.js
@@ -570,10 +570,10 @@ function renderPostWithNewVideoPlayer(post, user) {
         </video>
         <div class="video-overlay">
           <div class="play-button">
-            <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="24" cy="24" r="24" fill="rgba(244, 7, 82, 0.5)"/>
-              <path d="M32 24L20 32V16L32 24Z" fill="white"/>
-            </svg>
+           <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="24" cy="24" r="22" fill="rgba(244, 7, 82, 0.5)" stroke="white" stroke-width="3"/>
+             <path d="M32 24L20 32V16L32 24Z" fill="white"/>
+          </svg>
           </div>
           <div class="duration-badge">0:00</div>
         </div>
