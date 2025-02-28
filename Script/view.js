21d80584483b50ce2a68e5eda6ca34ceb006aaf6
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-02-28 14:50:39 +0100

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -599,10 +599,96 @@ function showDetail(postId) {
     if (commentTextarea) {
         commentTextarea.placeholder = `Reply to ${user.username}...`;
     }
-
     const hasVideo = post.video ? true : false;
+
     const hasImage = post.image ? true : false;
     
+      // Create video modal if it doesn't exist
+
+  if (!document.querySelector('.video-modal')) {
+
+    const videoModal = document.createElement('div');
+    videoModal.className = 'video-modal';
+    videoModal.innerHTML = `
+      <div class="modal-header">
+        <div class="back-button">
+          <img src="pics/backa.png" alt="Back">
+        </div>
+        <div class="modal-user-info">
+          <div class="user-avatar">
+            <img src="" alt="">
+          </div>
+          <div class="user-details">
+            <div class="username">
+              <span></span>
+              <img class="verify-badge" src="pics/verifi1.png">
+            </div>
+            <div class="timestamp"></div>
+          </div>
+        </div>
+        <div class="follow-button">Follow</div>
+      </div>
+      
+      <div class="video-player-container">
+        <video class="fullscreen-player">
+          <source src="" type="video/mp4">
+        </video>
+        
+        <div class="video-controls">
+          <div class="progress-container">
+            <div class="progress-bar">
+              <div class="progress-filled"></div>
+              <div class="progress-handle"></div>
+            </div>
+            <div class="time-display">0:00 / 0:00</div>
+          </div>
+          
+          <div class="control-buttons">
+            <div class="play-pause-btn">
+              <svg class="play-icon" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M8 5v14l11-7z" fill="white"/>
+              </svg>
+              <svg class="pause-icon" width="24" height="24" viewBox="0 0 24 24" style="display: none;">
+                <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" fill="white"/>
+              </svg>
+            </div>
+            <div class="volume-control">
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02z" fill="white"/>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="modal-content">
+        <p class="modal-post-text"></p>
+      </div>
+      
+      <div class="modal-actions">
+        <div class="action-buttons">
+          <div class="action-button">
+            <img src="pics/lovv.png" alt="Like">
+            <span>0</span>
+          </div>
+          <div class="action-button">
+            <img src="pics/chat.png" alt="Comment">
+            <span>0</span>
+          </div>
+          <div class="action-button">
+            <img src="pics/repost.png" alt="Repost">
+            <span>0</span>
+          </div>
+          <div class="action-button">
+            <img src="pics/naira.png" alt="Donate">
+            <span>0</span>
+          </div>
+        </div>
+      </div>
+    `;
+    document.body.appendChild(videoModal);
+  }
+
     postContent.innerHTML = `
         <div class="cust-name" data-post-id="${post.id}"> 
             <div class="heading">
@@ -633,102 +719,108 @@ function showDetail(postId) {
                     </div>
                 </div> 
             </div>
+       
 
-            <div>
-                <p class="foni" onclick="
-                    const foniElem = document.querySelector('.foni');
-                    if (foniElem.innerHTML === 'Follow') {
-                        foniElem.innerHTML = 'Following';
-                        foniElem.classList.add('follow');
-                    } else {
-                        foniElem.innerHTML = 'Follow';
-                        foniElem.classList.remove('follow');
-                    }
-                ">Follow</p>
+         <div>
+              <p class="foni" onclick="
+                const foniElem = document.querySelector('.foni');
+                
+                if (foniElem.innerHTML === 'Follow') {
+                  foniElem.innerHTML = 'Following';
+                  foniElem.classList.add('follow')
+                } else {
+                  foniElem.innerHTML = 'Follow';
+                  foniElem.classList.remove('follow')
+                }
+              ">Follow</p>
             </div>
 
-            <div class="dots">
-                <img class="dot" src="pics/duta.png">
-                <div class="tool">
-                    <p>More</p>
-                </div> 
-            </div>      
-        </div>
 
+            <div class="dots">
+              <img class="dot" src="pics/duta.png">
+              <div class="tool">
+                <p>More</p>
+              </div> 
+            </div>      
+          </div>
         <div class="tir">
             <p class="tiri">${post.content}<br></p>
         </div>
-
-        ${hasImage ? `
+        ${post.image ? `
         <div class="swet">
             <div class="laptop1">
-                <img class="lapto" src="${post.image}" loading="lazy">
+                <img class="lapto" src="${post.image}">
             </div>
         </div>
         ` : ''}
+        
 
-        ${hasVideo ? `
-        <div class="video-container laptop1">
-            <video class="video-thumbnail" preload="metadata" poster="${post.videoPoster || ''}">
-                <source src="${post.video}" type="video/mp4">
-            </video>
-            <div class="video-overlay">
-                <div class="play-button">
-                    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="24" cy="24" r="24" fill="rgba(244, 7, 82, 0.5)"/>
-                        <path d="M32 24L20 32V16L32 24Z" fill="white"/>
-                    </svg>
-                </div>
+            ${hasImage ? `
+
+            <div class="laptop1">
+                <img class="laptop" src="${post.image}" loading="lazy">
             </div>
-        </div>
-        ` : ''}
-
+            ` : ''}
+            
+            ${hasVideo ? renderPostWithNewVideoPlayer(post, user) : ''}
         <div class="lefto">
             <div class="dick">
-                <div>
-                    <p class="viewe"><span class="werey">615</span> reactions</p>
-                </div>
-                <div>
-                    <p class="viewe"><span class="werey">9</span> echoes</p>
-                </div>
+             <div>
+              <p class="viewe"><span class="werey">615</span> reactions</p>
+             </div>
+             <div>
+              <p class="viewe"><span class="werey">9</span> echoes</p>
+             </div>
             </div>
             <div class="twits">
-                <div>
-                    <img class="lefti" src="pics/stats.png">
-                </div>
-                <div>
-                    <p class="viewe">96.8K views</p>
-                </div>
+              <div>
+                <img class="lefti" src="pics/stats.png">
+              </div>
+              <div>
+                <p class="viewe">96.8K views</p>
+               </div>
             </div>
-        </div>
+          </div>
+          <div class="reaction">
+            <div class="small-photo1">
+              <a class="lino" href="Retail-Desktop-OtherUsers.html"><img class="hui" src="pics/man3.webp"></a>
+              <div class="vrea">
+                <img class="luve" src="pics/lovv.png">
+              </div>
+            </div>   
+            
+            <div class="small-photo1">
+              <a class="lino" href="Retail-Desktop-OtherUsers.html"><img class="hui" src="pics/girl2.webp"></a>
+              <div class="vrea">
+                <img class="luve" src="pics/lovv.png">
+              </div>
+            </div>   
+
+            <div class="small-photo1">
+              <a class="lino" href="Retail-Desktop-OtherUsers.html"><img class="hui" src="pics/man4.jpg"></a>
+              <div class="vrea">
+                <img class="luve" src="pics/2.gif">
+              </div>
+            </div>   
+
+            <div class="small-photo1">
+              <a class="lino" href="Retail-Desktop-OtherUsers.html"><img class="hui" src="pics/mypics.jpg"></a>
+              <div class="vrea">
+                <img class="luve" src="pics/lovv.png">
+              </div>
+            </div>   
+
+            <div class="small-photo1">
+              <a class="lino" href="Retail-Desktop-OtherUsers.html"><img class="hui" src="pics/pico8.webp"></a>
+              <div class="vrea">
+                <img class="luve" src="pics/3.gif">
+              </div>
+            </div>   
+          </div>
     `;
 
     switchPage("meal");
-
-    // Fix: Add video play functionality in post detail
-    if (hasVideo) {
-        const videoContainer = document.querySelector(".video-container");
-        const videoPlayer = videoContainer.querySelector(".video-thumbnail");
-        const playButton = videoContainer.querySelector(".play-button");
-
-        videoContainer.addEventListener("click", () => {
-            if (videoPlayer.paused) {
-                videoPlayer.play();
-                playButton.style.display = "none";
-            } else {
-                videoPlayer.pause();
-                playButton.style.display = "block";
-            }
-        });
-
-        videoPlayer.addEventListener("play", () => {
-            playButton.style.display = "none";
-        });
-
-        videoPlayer.addEventListener("pause", () => {
-            playButton.style.display = "block";
-        });
-    }
+    initializeVideoPlayers();
 }
 
 
