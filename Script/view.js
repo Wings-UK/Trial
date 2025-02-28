e195c4e91da6e6838eaace8a31c9c3f69420ea63
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-02-28 18:44:54 +0100

    News

--- Script/view.js
+++ Script/view.js
@@ -434,63 +434,6 @@ function openVideoModal(post) {
   history.pushState({ modalOpen: true }, '', window.location.href);
 }
 
-// Volume Control Code
-const volumeIcon = document.querySelector('.volume-control');
-const videolayer = document.querySelector('.fullscreen-player');
-
-volumeIcon.addEventListener('click', () => {
-    if (videolayer.muted) {
-        videolayer.muted = false; // Unmute the video
-        volumeIcon.innerHTML = `
-            <svg width="24" height="24" viewBox="0 0 24 24">
-                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02z" fill="white"/>
-            </svg>
-        `;
-    } else {
-        videolayer.muted = true; // Mute the video
-        volumeIcon.innerHTML = `
-            <svg width="24" height="24" viewBox="0 0 24 24">
-                <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z" fill="white"/>
-            </svg>
-        `;
-    }
-});
-
-// Update the volume icon on page load
-window.addEventListener('load', () => {
-    if (videolayer.muted) {
-        volumeIcon.innerHTML = `
-            <svg width="24" height="24" viewBox="0 0 24 24">
-                <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z" fill="white"/>
-            </svg>
-        `;
-    } else {
-        volumeIcon.innerHTML = `
-            <svg width="24" height="24" viewBox="0 0 24 24">
-                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02z" fill="white"/>
-            </svg>
-        `;
-    }
-});
-
-// Update the volume icon on page load
-window.addEventListener('load', () => {
-    if (videolayer.muted) {
-        volumeIcon.innerHTML = `
-            <svg width="24" height="24" viewBox="0 0 24 24">
-                <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z" fill="white"/>
-            </svg>
-        `;
-    } else {
-        volumeIcon.innerHTML = `
-            <svg width="24" height="24" viewBox="0 0 24 24">
-                <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02z" fill="white"/>
-            </svg>
-        `;
-    }
-});
-
-
 window.addEventListener('popstate', () => {
     const modal = document.querySelector('.video-modal');
     if (modal.classList.contains('active')) {
