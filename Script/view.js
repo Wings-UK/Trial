abcdee107627c7a1e2043be9a43ac89fc7bab2fb
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-03-01 07:03:05 +0100

    People

--- Script/view.js
+++ Script/view.js
@@ -969,7 +969,7 @@ function adjustVideoPlayer(videoElement) {
       videoElement.style.width = '100%';
       videoElement.style.height = 'auto';
       // Center vertically 
-      videoElement.style.top = '50%';
+      videoElement.style.top = '0';
       videoElement.style.transform = 'translateY(-50%)';
     }
   } else {
