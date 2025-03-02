69ace88dc6bcb39daa9b0cd3ad834d8959ed75df
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-03-02 11:34:18 +0100

    Good

--- Script/view.js
+++ Script/view.js
@@ -1128,13 +1128,12 @@ function renderUserPosts(userId) {
                   <img src="${post.image}">
                   ` : ''}
                   ${post.video ? `
-                 <div class="power">
+                <div class="video-container" data-post-id="${post.id}">
                     <video class="video-thumbnail" preload="metadata" poster="${post.videoPoster || ''}">
                       <source src="${post.video}" type="video/mp4">
-                       <div class="video-overlay">
-
+                    </video>
+                    <div class="video-overlay">
                       <div class="play-button">
-
                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <circle cx="24" cy="24" r="22" fill="rgba(244, 7, 82, 0.5)" stroke="white" stroke-width="3"/>
                           <path d="M34 24L18 34V14L34 24Z" fill="white"/>
@@ -1142,9 +1141,7 @@ function renderUserPosts(userId) {
                       </div>
                       <div class="duration-badge">0:00</div>
                     </div>
-                    </video>
-                   
-                  </div>
+                </div>
                   ` : ''}
                   <div class="contentma">
                     <p class="partner">${shortenText(post.content, textLimit, false)}</p>
