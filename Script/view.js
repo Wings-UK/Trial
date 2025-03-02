cd086c36ea7088e5b065fd93c565609752c71037
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-03-02 10:27:51 +0100

    Did you work?

--- Script/view.js
+++ Script/view.js
@@ -1130,11 +1130,11 @@ function renderUserPosts(userId) {
                   ${post.video ? `
                   <video class="video-thumbnail power" preload="metadata" poster="${post.videoPoster || ''}">
                     <source src="${post.video}" type="video/mp4">
-                    <div class="playerx"> 
-                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                             <circle cx="24" cy="24" r="22" fill="rgba(244, 7, 82, 0.5)" stroke="white" stroke-width="3"/>
-                             <path d="M34 24L18 34V14L34 24Z" fill="white"/>
-                       </svg>
+                    <div class="playerx" style="position: absolute; top: 10px; right: 10px;"> 
+                    <svg width="32" height="32" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="24" cy="24" r="22" fill="rgba(244, 7, 82, 0.5)" stroke="white" stroke-width="3"/>
+                    <path d="M34 24L18 34V14L34 24Z" fill="white"/>
+                    </svg>
                     </div>
                   </video>
                   ` : ''}
