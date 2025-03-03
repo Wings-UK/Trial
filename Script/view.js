5f9f3186904007a3f9608c00b7c0fd17d9b14ea9
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-03-03 23:50:22 +0100

    Acoustic

--- Script/view.js
+++ Script/view.js
@@ -1406,9 +1406,15 @@ function showMyProfile() {
         </div>
       </div>
   `;
-  document.querySelector(".wing").style.display = "block";
-  // Render posts by checking the global posts array for matching userId
-  renderUserPosts(user.id);
+  const wingDiv = document.querySelector(".wing");
+    if (wingDiv) {
+        wingDiv.style.display = "block";
+    }
+
+    setTimeout(() => {
+        renderUserPosts(user.id);
+    }, 100);
+}
   
   // Add event listener for edit profile button
   const editProfileBtn = document.querySelector('.edit-profile-btn');
