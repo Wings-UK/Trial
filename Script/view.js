dfda53b85b85d6d7e4967aa300c81a856a009fd4
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-03-04 07:23:03 +0100

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -1105,6 +1105,13 @@ function showUserProfile(userId) {
     lastScrollPos = window.pageYOffset || document.documentElement.scrollTop;
   // Attach scroll event listener
     window.addEventListener('scroll', handleScroll);
+    
+    
+    // **Force hide the wing div when visiting other profiles**
+    const wingDiv = document.querySelector(".wing");
+    if (wingDiv) {
+        wingDiv.style.display = "none";
+    }
     renderUserPosts(userId);
     setupScrollHandling();
     
