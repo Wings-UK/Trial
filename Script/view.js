d59cf6539ab26ee9833da675c8627f64caf67b49
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-03-09 17:12:25 +0100

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -1340,7 +1340,7 @@ function showUserProfile(userId) {
             </div>
           </div>
     `;
-    originalSwitchPage("profile");
+    switchPage("profile");
     
     lastScrollPos = window.pageYOffset || document.documentElement.scrollTop;
   // Attach scroll event listener
