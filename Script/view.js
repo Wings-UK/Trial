0127b5fb7785c25dd3e1b75d73686798efec9146
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-03-10 23:52:51 +0100

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -382,6 +382,7 @@ function renderHomepage() {
 // Add the following CSS to your stylesheet
 const heartStyle = `
 .heart-ai {
+ gap: 3px;
   width: 60px;
   display: flex;
   align-items: center;
@@ -491,7 +492,7 @@ justify-content: space-between;
   display: flex;
   width: 60px;
   align-items: center;
-  gap: 6px;
+  gap: 3px;
   cursor: pointer;
   font-size: 14px;
   font-family: ibm plex sans, roboto;
