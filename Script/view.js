13b6ef4a90a90803555ee1b3e62420c13b40acd0
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-03-18 00:47:07 +0100

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -374,7 +374,7 @@ function renderHomepage() {
 
     postContainer.innerHTML += postHTML;
   });
-
+  initializeVideoPlayers();
   // Initialize heart reaction functionality after rendering posts
   initializeHeartReactions();
 }
