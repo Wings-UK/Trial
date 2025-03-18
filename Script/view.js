ff688d02775130742286b53d227718b306e2d1df
Jeremiah Abraham <appen.jeremiah@gmail.com>
2025-03-18 08:32:26 +0100

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -158,6 +158,13 @@ const posts = [
       image: "pics/pico7.webp",
       content: "My guy is 18 with 0 experience, I got lil past, and it bothers him every time. I like him a lot, but what should I do ladies?",
     },
+    {
+      id: 13,
+      userId: 3,  // Refers to user with id 2 (@lena)
+      timestamp: "8 hours ago",
+      date: "Feb 28, 2025 3:56 PM",
+      content: "Shutdown Rivers Assembly. Declare State of Emergency in Rivers State - Obasanjo Visits SIM Fubara. Withdraw security personnel from all Rivers Assembly members. Shut down NNPC, shut down NDDC, and dismantle any forces that may rise against you. You are the governor, elected by the people. You are the Chief Security Officer of Rivers State. What you order stands—neither Tinubu nor Wike put you in power. Demolish any hall or arena set to host your impeachment. Tinubu needs Rivers votes to win the 2027 election, and you stand in his way. Stop playing U.S. politics in a country like this—there is no law here, everything is lawless. Obasanjo Visits Sim Fubara, Advises Him on How to Handle Wike and Tinubu.",
+    },
   
 ];
 
