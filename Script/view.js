23f934b946facced23b52df567d4cce34bcfea8b
Wings-UK <jeremiahabraham47@gmail.com>
2025-03-21 17:06:07 +0100

    update view.js

--- Script/view.js
+++ Script/view.js
@@ -1311,10 +1311,10 @@ function adjustVideoPlayer(videoElement) {
   
   if (videoAspect < 1) {
     // Portrait video - fill height with 70px space at top
-    const availableHeight = containerHeight - 52;
+    const availableHeight = containerHeight - 57;
     videoElement.style.height = availableHeight + 'px';
-    videoElement.style.width = 'auto';
-    videoElement.style.top = '52px'; // Position below the header
+    videoElement.style.width = '100%';
+    videoElement.style.top = '57px'; // Position below the header
     
     // Center horizontally
     const newWidth = availableHeight * videoAspect;
