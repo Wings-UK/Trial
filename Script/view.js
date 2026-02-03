c0ad43a95d8851bae1b28f1662583b7d92572e08
Jeremiah Abraham <appen.jeremiah@gmail.com>
2026-02-03 19:33:43 +0000

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -261,7 +261,7 @@ function createPostElement(post) {
                 <div class="small-photo1">
                     <a class="lino" onclick="${isOwnPost ? 'showMyProfile()' : `showProfile('${post.userId}')`}">
                        <img class="small-photo" src="${post.avatar}">
-                            <div style="padding-bottom: 100%;"></div>
+                          
                         </div>
                     </a>
                 </div>
