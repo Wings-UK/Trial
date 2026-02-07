3e1c1e3e4cbec01963f6829c5b81ec12511dd259
Jeremiah Abraham <appen.jeremiah@gmail.com>
2026-02-07 23:20:04 +0000

    Update view.jstears

--- Script/view.js
+++ Script/view.js
@@ -1897,10 +1897,10 @@ async function loadLikeNotifications() {
             read,
             actor_id,
             post_id,
-            actor_username:users!actor_id (username),   // only fetch username for actor
+            actor_username:users!actor_id (username),   
             actor_avatar:users!actor_id (avatar),
             post_image:posts!post_id (image),
-            post_id_full:posts!post_id (id)             // to get post.id for navigation
+            post_id_full:posts!post_id (id)             
         `)
         .eq('user_id', currentUserId)
         .eq('type', 'like')
