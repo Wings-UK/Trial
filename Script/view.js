ca862393d583042db7bcbaa8ea397468889ae090
Jeremiah Abraham <appen.jeremiah@gmail.com>
2026-02-08 01:54:41 +0000

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -1955,7 +1955,7 @@ function createLikeNotificationElement(notif) {
         align-items: center;
         justify-content: space-between;
         padding: 12px 16px;
-        border: 1px solid #eee;
+        border: 0.5px solid #eee;
         border-radius: 10px;
         margin: 8px 12px;
         background: white;
