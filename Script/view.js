23dcbe6fd6aeb1b740e00b0244839b031e1ab6bb
Jeremiah Abraham <appen.jeremiah@gmail.com>
2026-02-16 06:29:08 +0000

    update Script/view.js

--- Script/view.js
+++ Script/view.js
@@ -888,7 +888,7 @@ async function showProfile(userId) {
                 ` : ''}  
                 <div class="contentma">  
     <p class="partner">
-        ${(post.content || '').substring(0, 80)}${(post.content || '').length > 80 ? '...' : ''}
+        ${(post.content || '').substring(0, 80)}${(post.content || '').length > 80 ? '...' :''}
     </p>  
 </div>
             `;  
